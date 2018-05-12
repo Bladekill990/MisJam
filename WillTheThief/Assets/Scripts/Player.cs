dd573b782232e1018676b45c9e970779f0b23349
Justin Ste. Marie <justinstemarie@gmail.com>
2018-05-11 23:29:20 -0400

    Player.cs stuff

---
+++ WillTheThief/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            print("interact key was pressed");
+        }
+
+        if (Input.GetKeyDown("w") || Input.GetKeyDown("a") || Input.GetKeyDown("s") || Input.GetKeyDown("d"))
+        {
+            movement();
+        }
+
+    }
+
+
+
+
+
+    void movement()
+    {
+        float vMove = ((int)(Input.GetKeyDown("w"))) * 1.0f + ((int)(Input.GetKeyDown("s"))) * -1.0f;
+        float hMove = ((int)(Input.GetKeyDown("a"))) * -1.0f + ((int)(Input.GetKeyDown("d"))) * 1.0f;
+
+        //playermovementvector = vector(playermovementvector.x+hMove, playermovementvector.y+vMove, playermovementvector.z);
+    }
+}
